ffc1278c33cec51f2b0fec038bafc784c0fc2a55
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-08-29 17:39:46 -0500

    coding #1 creating xCode project

---
+++ our_first_ios_app/our_first_ios_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  our_first_ios_app
+//
+//  Created by Josè Cruz Arroyo on 29/08/20.
+//  Copyright © 2020 Josè Cruz Arroyo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ our_first_ios_app/our_first_ios_appTests/our_first_ios_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  our_first_ios_appTests.swift
+//  our_first_ios_appTests
+//
+//  Created by Josè Cruz Arroyo on 29/08/20.
+//  Copyright © 2020 Josè Cruz Arroyo. All rights reserved.
+//
+
+import XCTest
+@testable import our_first_ios_app
+
+class our_first_ios_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
